05d0c04cf0e14091f28687719630b52799c0a97f
htungg <lehoangtung081003@gmail.com>
2024-04-29 13:01:25 +0700

    feat: wrapper for elastic search function

--- pkg/elasticsearch/dtos.go
+++ pkg/elasticsearch/dtos.go
@@ -1,16 +1,5 @@
 package elasticsearch
 
-import "time"
-
-type ResumeDTO struct {
-	ResumeID        string    `json:"resume_id"`
-	FullText        string    `json:"full_text"`
-	DownloadLink    string    `json:"download_link"`
-	VectorEmbedding string    `json:"vector_embedding"`
-	CreatedAt       time.Time `json:"created_at"`
-	UpdatedAt       time.Time `json:"updated_at"`
-}
-
 type ElasticResponse struct {
 	ID      string `json:"id"`
 	Content string `json:"content"`
