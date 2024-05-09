2800813081010662214ae4a7a1b6720653687552
htungg <lehoangtung081003@gmail.com>
2024-05-09 17:27:45 +0700

    bug: fix db connection docker but error when reading file viper

--- backend/pkg/cfg/viper.go
+++ backend/pkg/cfg/viper.go
@@ -3,23 +3,24 @@ package cfg
 import (
 	"fmt"
 	"github.com/spf13/viper"
+	"log"
 	"os"
 )
 
 func SetupConfig() {
-	//viper.AddConfigPath("./statics")
+	viper.AddConfigPath("./statics")
 	env := os.Getenv("ENVIRONMENT")
 
 	fmt.Printf("CURRENT ENV: %s\n", env)
 
-	//viper.SetConfigName("config.default") // config.toml
+	viper.SetConfigName("config.default") // config.toml
 	viper.AutomaticEnv()
 
-	//if err := viper.ReadInConfig(); err != nil {
-	//	if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-	//		log.Fatalf("Config file not found")
-	//	} else {
-	//		log.Fatalf("Errorf reading conf file %s", err.Error())
-	//	}
-	//}
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			log.Fatalf("Config file not found")
+		} else {
+			log.Fatalf("Errorf reading conf file %s", err.Error())
+		}
+	}
 }
