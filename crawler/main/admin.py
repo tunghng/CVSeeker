e036d8f6eefa22333a269403cd8fc6ac4add537f
TungHamHoc <110160843+tunghng@users.noreply.github.com>
2024-05-22 17:19:40 +0700

    Fix: fix directory for merge

--- crawler/main/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Scrapin, RelevanceAI, Phantombuster, ProviderManagement
-
-admin.site.register(Scrapin)
-admin.site.register(RelevanceAI)
-admin.site.register(Phantombuster)
-admin.site.register(ProviderManagement)
--- crawler/main/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from . import views
-urlpatterns = [ 
-    path("", view=views.index, name="index"),
-    path("api/getfulltext/", view=views.GetFulltext.as_view(), name="Get Full Text")
-]
